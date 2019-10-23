8b6c6350862b73d8c5dc66981879ff1ecdbd5eb2
fenglin098 <feng.lin098@gmail.com>
2019-10-23 16:18:45 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+public class Main {
+    public static void main (String[] args){
+        int roll1, roll2, count, sum;
+        count=0;
+        do{
+            Random num=new Random();
+            roll1=1+num.nextInt(1);
+            roll2=1+num.nextInt(1);
+            sum=roll1+roll2;
+            if (roll1==roll2) {
+                count++;
+                System.out.println("User rolls "+roll1+" & "+roll2+"\nDOUBLES! \nMove" + sum + " Spaces and roll again getting...");
+                if (count==3);{
+                    System.out.println("You go to jail!");
+                    break;
+                }
+            }else if(roll1!=roll2){
+                System.out.println("User rolls "+roll1+" & "+roll2+"\nMove "+sum+" Spaces and stop");
+            }
+        }while(roll1==roll2);
+
+    }
+}
